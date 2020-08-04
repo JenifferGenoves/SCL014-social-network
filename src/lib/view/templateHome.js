18a7199f485cb17a14acb2e9669c9c4460349011
Jeniffer Genoves <jenifer.genoves03@gmail.com>
2020-08-04 13:34:44 -0400

    Firebase

--- src/lib/view/templateHome.js
+++ src/lib/view/templateHome.js
@@ -1,11 +1,18 @@
+import { login } from '../index.js';
 export const home = () => {
     const divHome = document.createElement('div');
 
     const viewHome = `
     <h1>Hola mundirijillo</h1>
+    <button id='loginGoogle'>Login con Google</buttom>
     `
 
     divHome.innerHTML =viewHome;
+
+    const btn =divHome.querySelector('#loginGoogle');
+    btn.addEventListener('click', () => {
+        login()
+    })
  
     return divHome;
 
