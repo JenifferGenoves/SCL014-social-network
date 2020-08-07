1dc05570a5284c299ac1b574a7c89b24566c9d5e
JenifferGenoves <62884668+JenifferGenoves@users.noreply.github.com>
2020-08-07 18:24:38 -0400

    Revert "arreglo"

--- src/lib/view/RecoverpassTemplate.js
+++ src/lib/view/RecoverpassTemplate.js
@@ -1,7 +1,6 @@
 /* Aquí va recuperación de contraseña */
 
 export const recover = () => {
-  const divRecover = document.createElement('div');
   const viewRecover = `
 <h1>Escribe tú correo electrónico y te enviaremos un link para reestablecer tu contraseña</h1>
 <br> <br>
@@ -9,6 +8,5 @@ input type="email" id="email" class="infoBox" required></input>
 <br> <br>
 <input type="submit" id="resendpass" value="Enviar"></input>
 `;
-  divRecover.innerHTML = viewRecover;
-  return divRecover;
+  return viewRecover;
 };
